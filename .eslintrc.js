f0df42b456cd4cafaec0739e61ffcc1c85dc24ee
Ilya <squdward13@gmail.com>
2024-05-09 12:01:21 +0300

    ✨ Fix tabs in eslintrc file ✨

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,62 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:eslint-comments/recommended",
-    "plugin:promise/recommended",
-    "plugin:import/recommended",
-    "plugin:prettier/recommended",
-  ],
-  plugins: ["simple-import-sort", "prettier"],
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: "latest",
-  },
-  rules: {
-    quotes: ["warn", "double"],
-    "promise/prefer-await-to-then": "warn",
-    "react/function-component-definition": "off",
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"airbnb",
+		"airbnb/hooks",
+		"plugin:eslint-comments/recommended",
+		"plugin:promise/recommended",
+		"plugin:import/recommended",
+		"plugin:prettier/recommended",
+	],
+	plugins: ["simple-import-sort", "prettier"],
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: "latest",
+	},
+	rules: {
+		quotes: ["warn", "double"],
+		"promise/prefer-await-to-then": "warn",
+		"react/function-component-definition": "off",
 
-    // Import rules
-    "simple-import-sort/imports": [
-      "error",
-      {
-        groups: [
-          // External packages:
-          ["^react", "^@?\\w"],
+		// Import rules
+		"simple-import-sort/imports": [
+			"error",
+			{
+				groups: [
+					// External packages:
+					["^react", "^@?\\w"],
 
-          // Alias imports
-          ["^@?\\w"],
+					// Alias imports
+					["^@?\\w"],
 
-          // Side effect imports:
-          ["^\\u0000"],
+					// Side effect imports:
+					["^\\u0000"],
 
-          // Parent imports:
-          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+					// Parent imports:
+					["^\\.\\.(?!/?$)", "^\\.\\./?$"],
 
-          // Other relative imports:
-          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+					// Other relative imports:
+					["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
 
-          // Style imports:
-          ["^.+\\.s?css$"],
-        ],
-      },
-    ],
-    "simple-import-sort/exports": "error",
-    "import/first": "error",
-    "import/newline-after-import": "error",
-    "import/no-duplicates": "error",
+					// Style imports:
+					["^.+\\.s?css$"],
+				],
+			},
+		],
+		"simple-import-sort/exports": "error",
+		"import/first": "error",
+		"import/newline-after-import": "error",
+		"import/no-duplicates": "error",
 
-    "prettier/prettier": [
-      "error",
-      {
-        // endOfLine: "auto",
-      },
-    ],
-  },
+		"prettier/prettier": [
+			"error",
+			{
+				// endOfLine: "auto",
+			},
+		],
+	},
 };
