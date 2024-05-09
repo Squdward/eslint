789ba9d8c836d236edf7dffd607ce74f93e38198
Ilya <squdward13@gmail.com>
2024-05-09 12:00:01 +0300

    ✨ Add eslint-plugin-import ✨

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,54 @@ module.exports = {
     "airbnb/hooks",
     "plugin:eslint-comments/recommended",
     "plugin:promise/recommended",
+    "plugin:import/recommended",
     "plugin:prettier/recommended",
   ],
-  plugins: ["prettier"],
+  plugins: ["simple-import-sort", "prettier"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
   rules: {
     quotes: ["warn", "double"],
     "promise/prefer-await-to-then": "warn",
     "react/function-component-definition": "off",
+
+    // Import rules
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          // External packages:
+          ["^react", "^@?\\w"],
+
+          // Alias imports
+          ["^@?\\w"],
+
+          // Side effect imports:
+          ["^\\u0000"],
+
+          // Parent imports:
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+
+          // Other relative imports:
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+
+          // Style imports:
+          ["^.+\\.s?css$"],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+
+    "prettier/prettier": [
+      "error",
+      {
+        // endOfLine: "auto",
+      },
+    ],
   },
 };
