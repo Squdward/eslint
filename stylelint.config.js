925718af16f5a26af3c3f7f737e46ad3b599a269
Ilya <squdward13@gmail.com>
2025-05-08 05:16:00 +0300

    add stylelint config

---
+++ stylelint.config.js
@@ -0,0 +1,13 @@
+/** @type {import('stylelint').Config} */
+export default {
+	extends: [
+		"stylelint-config-standard",
+		"stylelint-config-recommended",
+		"stylelint-config-recess-order"
+	],
+
+	plugins: [
+		"stylelint-order",
+		"stylelint-scss"
+	]
+}
