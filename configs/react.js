3442560b69ae74523ea3dd6f2d30cc3eef9be30f
Ilya <squdward13@gmail.com>
2025-05-10 21:57:47 +0300

    V1.2.0

--- configs/react.js
+++ configs/react.js
@@ -5,35 +5,38 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 
 export default tseslint.config([
-    {
-        files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
-        languageOptions: {
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
-            globals: {
-                ...globals.browser,
-            },
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-        plugins: {
-            react: reactPlugin,
-            "react-hooks": reactHooksPlugin,
-            "react-refresh": reactRefresh
-        },
-        rules: {
-            ...reactPlugin.configs.recommended.rules,
-            ...reactHooksPlugin.configs["recommended-latest"].rules,
-            "react-hooks/rules-of-hooks": "error",
-            "react-hooks/exhaustive-deps": "warn",
-            "react/jsx-no-useless-fragment": "warn",
-            "react/self-closing-comp": ["warn", {
-                "html": true,
-                "component": true
-            }],
-            "react/react-in-jsx-scope": "off",
-            "react/prop-types": "off",
-        },
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs["recommended-latest"].rules,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/jsx-no-useless-fragment": "warn",
+      "react/self-closing-comp": [
+        "warn",
+        {
+          html: true,
+          component: true,
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
 ]);
---
+++ main.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default defineConfig([
+  // Базовые настройки Eslint JS
+  eslint.configs.recommended,
+
+  // Подключение правил Prettier через плагин
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      ...prettierConfig.rules,
+    },
+  },
+]);
